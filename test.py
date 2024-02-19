f2bb55369c0ed27a1aa35141f5a01d057606c806
Rumaisa Ilyas <i200664@nu.edu.pk>
2024-02-19 10:33:54 +0500

    First Commit

---
+++ test.py
@@ -0,0 +1,20 @@
+import pytest
+from main import StudentsInMLOps
+
+def test_enrollStudents():
+    ops = StudentsInMLOps()
+    ops.enrollStudents(5)
+    assert ops.getTotalStrength() == 5
+
+def test_dropStudents():
+    ops = StudentsInMLOps()
+    ops.enrollStudents(10)
+    ops.dropStudents(3)
+    assert ops.getTotalStrength() == 7
+
+def test_getClassName():
+    ops = StudentsInMLOps()
+    assert ops.getClassName() == "StudentsInMLOps"
+
+if __name__ == "__main__":
+    pytest.main()
